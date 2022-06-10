9477c162bea68b2b55c5d5894ac411301eb72665
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-06-11 03:58:11 +0700

    Remove side effect from User component, add default image to the user

--- src/components/Users/UserCard/UserCard.jsx
+++ src/components/Users/UserCard/UserCard.jsx
@@ -1,11 +1,12 @@
 import classes from './UserCard.module.css';
+import defaultUserAvatar from '../../../assets/images/default-user-avatar.png'
 
 const UserCard = (props) => {
   return (
     <div className={classes.userCard}>
       <div className={classes.userCard__left}>
         <div className={classes.userCard__image}>
-          <img src={props.avatar} alt="user avatar" />
+          <img src={props.photos.small !== null ? props.photos.small : defaultUserAvatar} alt="user avatar" />
         </div>
         <div className={classes.userCard__follow}>
           {props.followed === false
@@ -16,12 +17,12 @@ const UserCard = (props) => {
       </div>
       <div className={classes.userCard__right}>
         <div className={classes.userCard__profile_info}>
-          <span className={classes.userCard__fullName}>{props.fullName}</span>
+          <span className={classes.userCard__fullName}>{props.name}</span>
           <span className={classes.userCard__status}>{props.status}</span>
         </div>
         <div className={classes.userCard__profile_location}>
-          <span>{props.country}</span>
-          <span>{props.city}</span>
+          <span>{'props.country'}</span>
+          <span>{'props.city'}</span>
         </div>
       </div>
     </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,34 +1,34 @@
 import UserCard from "./UserCard/UserCard";
 import classes from './User.module.css';
+import axios from "axios";
 
 const Users = (props) => {
-  // debugger
-  if (props.users.length === 0) {
-    props.setUsers([
-      { id: 1, followed: false, avatar: 'https://chpic.su/_data/stickers/p/Pepefliperskaya/Pepefliperskaya_001.webp', fullName: 'Ivan Bondarev', location: { country: 'Russia', city: 'Barnaul' }, status: 'I\'m fine' },
-      { id: 2, followed: true, avatar: 'https://chpic.su/_data/stickers/p/Pepefliperskaya/Pepefliperskaya_001.webp', fullName: 'Oleg Necheporenko', location: { country: 'Russia', city: 'Barnaul' }, status: 'I\'m fine' },
-      { id: 3, followed: false, avatar: 'https://chpic.su/_data/stickers/p/Pepefliperskaya/Pepefliperskaya_001.webp', fullName: 'Barry Allen', location: { country: 'Russia', city: 'Barnaul' }, status: 'I\'m fine' }
-    ])
+
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => props.setUsers(response.data.items))
+    }
   }
+
 
   let users = props.users.map(u => {
 
     return <UserCard
       key={u.id}
       id={u.id}
-      fullName={u.fullName}
-      country={u.location.country}
-      city={u.location.city}
+      name={u.name}
       status={u.status}
       followed={u.followed}
-      avatar={u.avatar}
+      photos={u.photos}
       follow={props.follow}
     />
   })
   return (
     <div className={classes.users}>
       {users}
-      <button>SHOW MORE</button>
+      <button onClick={getUsers}>SHOW MORE</button>
     </div>
   )
 }
