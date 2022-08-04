a3687898d7cd7f15ea7495cd42c9ccb0e9ce783e
Ivan Bondarev <ivanboarthrills@gmail.com>
2022-08-04 21:07:45 +0700

    Add follow, unfollow functions through API

--- src/components/Users/UserCard/UserCard.jsx
+++ src/components/Users/UserCard/UserCard.jsx
@@ -1,38 +1,68 @@
 import classes from './UserCard.module.css';
 import defaultUserAvatar from '../../../assets/images/default-user-avatar.png'
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 const UserCard = (props) => {
-  return (
-    <div className={classes.userCard}>
-      <div className={classes.userCard__left}>
-        <NavLink to={`/profile/${props.id}`}>
-          <div className={classes.userCard__image}>
-            <img src={props.photos.small !== null ? props.photos.small : defaultUserAvatar} alt="user avatar" />
-          </div>
-        </NavLink>
-        <div className={classes.userCard__follow}>
-          {props.followed === false
-            ? <button onClick={() => { props.follow(props.id) }} className='primary_button'>Follow</button>
-            : <button onClick={() => { props.follow(props.id) }} className='primary_button'>Unfollow</button>
-          }
-        </div>
-      </div>
-      <div className={classes.userCard__right}>
-        <div className={classes.userCard__profile_info}>
-          <span className={classes.userCard__fullName}>{props.name}</span>
-          <span className={classes.userCard__status}>{props.status}</span>
-        </div>
-        {props.country || props.city ?
-          <div className={classes.userCard__profile_location}>
-            <span>{props.country}</span>
-            <span>{props.city}</span>
-          </div>
-          : ''}
+	return (
+		<div className={classes.userCard}>
+			<div className={classes.userCard__left}>
+				<NavLink to={`/profile/${props.id}`}>
+					<div className={classes.userCard__image}>
+						<img src={props.photos.small !== null ? props.photos.small : defaultUserAvatar} alt="user avatar" />
+					</div>
+				</NavLink>
+				<div className={classes.userCard__follow}>
+					{props.followed === false
+						? <button onClick={() => {
+							axios
+								.post(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {}, {
+									withCredentials: true,
+									headers: {
+										"API-KEY": '20f5c211-d0a1-474c-a715-82c47d390824'
+									}
+								})
+								.then(response => {
+									if (response.data.resultCode === 0) {
+										props.follow(props.id);
+									}
+								})
 
-      </div>
-    </div>
-  )
+
+						}} className='primary_button'>Follow</button>
+						: <button onClick={() => {
+							axios
+								.delete(`https://social-network.samuraijs.com/api/1.0/follow/${props.id}`, {
+									withCredentials: true,
+									headers: {
+										"API-KEY": '20f5c211-d0a1-474c-a715-82c47d390824'
+									}
+								})
+								.then(response => {
+									if (response.data.resultCode === 0) {
+										props.follow(props.id)
+
+									}
+								})
+						}} className='primary_button'>Unfollow</button>
+					}
+				</div>
+			</div>
+			<div className={classes.userCard__right}>
+				<div className={classes.userCard__profile_info}>
+					<span className={classes.userCard__fullName}>{props.name}</span>
+					<span className={classes.userCard__status}>{props.status}</span>
+				</div>
+				{props.country || props.city ?
+					<div className={classes.userCard__profile_location}>
+						<span>{props.country}</span>
+						<span>{props.city}</span>
+					</div>
+					: ''}
+
+			</div>
+		</div>
+	)
 }
 
 export default UserCard;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -21,7 +21,9 @@ class UsersContainer extends React.Component {
 		if (this.props.users.length === 0) {
 			this.props.toggleIsFetching(true)
 			axios
-				.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+				.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+					withCredentials: true,
+				})
 				.then(response => {
 					this.props.setUsers(response.data.items)
 					this.props.setTotalUsersCount(response.data.totalCount)
@@ -35,7 +37,9 @@ class UsersContainer extends React.Component {
 		this.props.setCurrentPage(pageNumber)
 		this.props.toggleIsFetching(true)
 		axios
-			.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+			.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+				withCredentials: true
+			})
 			.then(response => {
 				this.props.setUsers(response.data.items)
 				this.props.toggleIsFetching(false)
